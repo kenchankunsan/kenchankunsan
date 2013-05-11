f307805d3453c72da01d8d98a6b578632ce6a7ac
Hiroshi Yoshida <hrysd22@gmail.com>
2013-05-11 10:41:41 +0900

    Add tweet button

--- kenchankunsan.rb
+++ kenchankunsan.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
+require 'haml'
 
 get '/' do
-  ken = 'けん'
+  @ken = 'けん'
   suffixes = %w(さん くん ちゃん)
 
-  while (ken + (suffix = suffixes.sample)).size < 140 do
-    ken << suffix
+  while (@ken + (suffix = suffixes.sample)).size < 140 do
+    @ken << suffix
   end
 
-  ken + '…'
+  @ken + '…'
+
+  haml :kenchan
 end
